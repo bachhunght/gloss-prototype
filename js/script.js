ed7027957c359c659876bc114eedf7b53098de41
hunght <bachhunght@gmail.com>
2016-09-12 14:44:54 +0700

    update style

--- js/script.js
+++ js/script.js
@@ -13,51 +13,42 @@ $(function() {
     }
   });
 });
-function openCity(evt, cityName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
 
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-
-    // Show the current tab, and add an "active" class to the link that opened the tab
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
-}
-/* Toggle between adding and removing the "active" and "show" classes when the user clicks on one of the "Section" buttons. The "active" class is used to add a background color to the current button when its belonging panel is open. The "show" class is used to open the specific accordion panel */
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-    acc[i].onclick = function(){
-        this.classList.toggle("active");
-        this.nextElementSibling.classList.toggle("show");
-    }
-}
 $(document).ready(function () {
-  $('#calendar').datepicker({
-    inline: true,
-    firstDay: 1,
-    showOtherMonths: true,
-    dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+
+  $('.menu-icon').click(function(){
+    $(this).toggleClass('active');
+    $(this).next().toggleClass('active');
   });
 
-  $( ".ui-state-highlight" ).after( '<div class="date-times"><div class="date-times-content"><div class="time">9:00am</div><div class="time">10:00am</div><div class="time">11:00am</div><div class="time">12:00am</div><div class="time active">2:00pm</div><div class="time">4:00pm</div><div class="time">5:00pm</div><div class="time">6:00pm</div></div></div>' );
-  $('.ui-datepicker-calendar').
-  $( ".ui-state-highlight" ).click(function() {
-    if ($(this).next().hasClass('date-times')) {
-      $(this).addClass('ok');
-      $(this).next().addClass('ok');
-    }
-    $(this).next().addClass('oktotes');
-  });
+  function openCity(evt, cityName) {
+      // Declare all variables
+      var i, tabcontent, tablinks;
+
+      // Get all elements with class="tabcontent" and hide them
+      tabcontent = document.getElementsByClassName("tabcontent");
+      for (i = 0; i < tabcontent.length; i++) {
+          tabcontent[i].style.display = "none";
+      }
+
+      // Get all elements with class="tablinks" and remove the class "active"
+      tablinks = document.getElementsByClassName("tablinks");
+      for (i = 0; i < tablinks.length; i++) {
+          tablinks[i].className = tablinks[i].className.replace(" active", "");
+      }
+
+      // Show the current tab, and add an "active" class to the link that opened the tab
+      document.getElementById(cityName).style.display = "block";
+      evt.currentTarget.className += " active";
+  }
+  /* Toggle between adding and removing the "active" and "show" classes when the user clicks on one of the "Section" buttons. The "active" class is used to add a background color to the current button when its belonging panel is open. The "show" class is used to open the specific accordion panel */
+  var acc = document.getElementsByClassName("accordion");
+  var i;
+
+  for (i = 0; i < acc.length; i++) {
+      acc[i].onclick = function(){
+          this.classList.toggle("active");
+          this.nextElementSibling.classList.toggle("show");
+      }
+  }
 });
